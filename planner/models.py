5934eaad7f79a1bc2a02c400936d248df38ae479
guillaume <coigniez.g@gmail.com>
2023-12-03 22:17:08 +0100

    first commit

---
+++ planner/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    instructor = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    study_points = models.IntegerField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Chapter(models.Model):
+    title = models.CharField(max_length=255)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    parent_chapter = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    order = models.IntegerField(default=0)
+    content = models.TextField()
+    guessed_time = models.IntegerField(default=0)
+    time_spent = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['order',]
+        
+    def __str__(self):
+        return self.title
+        
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    time_spent = models.IntegerField(default=0)
+    time_estimated = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['order',]
+        
+    def __str__(self):
+        return self.title
+    
+class Deadline(models.Model):
+    title = models.CharField(max_length=255)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    description = models.TextField()
+    date = models.DateField()
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+class Milestone(models.Model):
+    title = models.CharField(max_length=255)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    chapters = models.ManyToManyField(Chapter)
+    tasks = models.ManyToManyField(Task)
+    description = models.TextField()
+    date = models.DateField()
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+
+    
---
+++ planner/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Document, Chapter, Task, Deadline, Milestone
+
+
+def home(request):
+    context = {
+        'documents': Document.objects.all()
+    }
+    return render(request, 'planner/home.html', context)
+
