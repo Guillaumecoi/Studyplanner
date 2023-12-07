d8d5364f10648b8e7d686d094041deb65be44ae1
guillaume <coigniez.g@gmail.com>
2023-12-07 11:38:08 +0100

    profile form

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -26,3 +27,30 @@ class UserRegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(username=email).exists() and email != self.instance.email:
+            raise ValidationError("Email already exists")
+        return email
+    
+    def save(self, commit=True):
+        user = super(UserUpdateForm, self).save(commit=False)
+        user.username = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+    
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
